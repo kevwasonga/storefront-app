5a228e802fe2579fd11f2a13e143379499a85266
kevwasonga <kevinwasonga116@gmail.com>
2025-05-27 17:01:44 +0300

    Feat: update  metro config to reflect the same directory aliases defined in babel.config

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,28 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+const babelConfig = require('./babel.config');
+
+const alias =
+  babelConfig.plugins.find(
+    (plugin) => Array.isArray(plugin) && plugin[0] === 'module-resolver'
+  )?.[1]?.alias || {};
+
+const extraNodeModules = Object.fromEntries(
+  Object.entries(alias).map(([key, relPath]) => [
+    key,
+    path.resolve(__dirname, relPath),
+  ])
+);
 
 module.exports = {
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: true,
-            },
-        }),
-    },
+  resolver: {
+    extraNodeModules,
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
 };
