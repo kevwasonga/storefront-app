b1227fd8aac72eec2c91889eba15717cda9ab7a9
kevwasonga <kevinwasonga116@gmail.com>
2025-05-27 16:49:24 +0300

    Fix navigation from Bootscreen to other screens

--- src/features/Core/CoreStack.js
+++ src/features/Core/CoreStack.js
@@ -3,17 +3,33 @@ import { createStackNavigator } from '@react-navigation/stack';
 import BootScreen from './screens/BootScreen';
 import StorefrontScreen from './screens/StorefrontScreen';
 import NetworkScreen from './screens/NetworkScreen';
+import { View, Text, Button } from 'react-native';
 
 const RootStack = createStackNavigator();
 
-const CoreStack = ({ route }) => {
-    return (
-        <RootStack.Navigator initialRouteName={'BootScreen'}>
-            <RootStack.Screen name="BootScreen" component={BootScreen} options={{ headerShown: false, animationEnabled: false, gestureEnabled: false }} />
-            <RootStack.Screen name="StorefrontScreen" component={StorefrontScreen} options={{ headerShown: false, animationEnabled: false, gestureEnabled: false }} />
-            <RootStack.Screen name="NetworkScreen" component={NetworkScreen} options={{ headerShown: false, animationEnabled: false, gestureEnabled: false }} />
-        </RootStack.Navigator>
-    );
+
+
+const CoreStack = () => {
+  return (
+    <RootStack.Navigator initialRouteName="BootScreen">
+     
+      <RootStack.Screen
+        name="BootScreen"
+        component={BootScreen}
+        options={{ headerShown: false, animationEnabled: false, gestureEnabled: false }}
+      />
+      <RootStack.Screen
+        name="StorefrontScreen"
+        component={StorefrontScreen}
+        options={{ headerShown: false, animationEnabled: false, gestureEnabled: false }}
+      />
+      <RootStack.Screen
+        name="NetworkScreen"
+        component={NetworkScreen}
+        options={{ headerShown: false, animationEnabled: false, gestureEnabled: false }}
+      />
+    </RootStack.Navigator>
+  );
 };
 
 export default CoreStack;
